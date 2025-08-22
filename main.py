86c3e3b96ccdc59b644a7c1054f75090bf50e678
seetvn <waiyansteven2002@gmail.com>
2025-08-22 16:46:02 +0400

    TODO: made query loop

--- main.py
+++ main.py
@@ -4,21 +4,22 @@ from agent.loop import Agent
 
 def parse_args():
     ap = argparse.ArgumentParser(description="PokeDeep – Reactive Pokédex Agent")
-    ap.add_argument("question", type=str, help="User request/query")
     ap.add_argument("--model", type=str, default=os.getenv("MODEL", "gpt-4o-mini"))
     ap.add_argument("--max-steps", type=int, default=6)
-    ap.add_argument("--temperature", type=float, default=0.2)
+    ap.add_argument("--temperature", type=float, default=0.4)
     return ap.parse_args()
 
-#TODO: make main() continuous
-#TODO: change setup of loop
+#TODO: add back query argument for one-off queries
 def main():
     args = parse_args()
     agent = Agent(model=args.model, max_steps=args.max_steps, temperature=args.temperature)
-    answer = agent.run(args.question)
-    print("\n" + "="*60 + "\nFINAL ANSWER:\n" + "="*60)
-    print(answer)
-    print()
+    while True:
+        question = input("\nWhat would you like to know? (or 'exit' to quit): \n").strip()
+        if question .lower() in ("exit", "quit"):
+            print("Goodbye!")
+            break
+        agent.run(question)
+
 
 if __name__ == "__main__":
     main()
